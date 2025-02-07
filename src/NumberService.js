83485e3a09c66d1d6ba11198987738efc6edf5c2
FERANMI <168641833+Feranmidevelops@users.noreply.github.com>
2025-02-07 16:33:35 +0100

    http error

--- src/NumberService.js
+++ src/NumberService.js
@@ -1,14 +1,14 @@
-
-import axios from 'axios';
+import axios from "axios";
 
 export const getNumberProperties = async (number) => {
-    try {
-      const response = await axios.get(`https://numbersapi.com/${number}/math?json`);
-      console.log("API Response:", response);  // Log the full response
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching number properties:", error);
-      return null;
-    }
-  };
-  
+  try {
+    const response = await axios.get(
+      `http://numbersapi.com/${number}/math?json`
+    );
+    console.log("API Response:", response); // Log the full response
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching number properties:", error);
+    return null;
+  }
+};
